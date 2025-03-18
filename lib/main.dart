6a9d439888c8987f31474af4c264856832673b0c
Irving Salazar <ISCIrvingDev@gmail.com>
2025-03-18 01:01:56 -0600

    Bussiness logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,6 +32,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // * Bussiness logic
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = '00', digitMinutes = '00', digitHours = '00';
+  Timer? timer;
+  bool started = false;
+  List<String> laps = [];
+
+  void stop() {
+    timer!.cancel();
+
+    setState(() {
+      started = false;
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+
+      digitSeconds = '00';
+      digitMinutes = '00';
+      digitHours = '00';
+
+      started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = '$digitHours:$digitMinutes:$digitSeconds';
+
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void start() {
+    started = true;
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+
+      if (localSeconds > 59) {
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        } else {
+          localMinutes++;
+          localSeconds = 0;
+        }
+      }
+
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+
+        digitSeconds = seconds >= 10 ? '$seconds' : '0$seconds';
+        digitMinutes = minutes >= 10 ? '$minutes' : '0$minutes';
+        digitHours = hours >= 10 ? '$hours' : '0$hours';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
