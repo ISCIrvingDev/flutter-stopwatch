3b31785dbb152f8876ad90d31a70910a4619afef
Irving Salazar <ISCIrvingDev@gmail.com>
2025-03-18 00:47:33 -0600

    Maquetacion

--- lib/main.dart
+++ lib/main.dart
@@ -13,53 +13,100 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'StopWatch'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0XFF1c2757),
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: const Color(0XFF1c2757),
+        title: Text(widget.title, style: TextStyle(color: Colors.white)),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  '00:00:00',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 82,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Color(0XFF323F68),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: RawMaterialButton(
+                      shape: const StadiumBorder(
+                        side: BorderSide(color: Colors.blue),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Start',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(width: 8),
+                  IconButton(
+                    color: Colors.white,
+                    onPressed: () {},
+                    icon: Icon(Icons.flag),
+                  ),
+                  SizedBox(width: 8),
+
+                  Expanded(
+                    child: RawMaterialButton(
+                      fillColor: Colors.blue,
+                      shape: const StadiumBorder(
+                        side: BorderSide(color: Colors.blue),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Reset',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
